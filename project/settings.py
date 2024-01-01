390647037cfcc3736aaa0f3d96cd0bb1f6fa9ee0
savioomio <saviopessoaafonso@gmail.com>
2024-01-01 05:06:45 -0300

    configurando ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -105,9 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 #(para o sistema em produção)
 ALLOWED_HOSTS = ['https://prepccjsom-production.up.railway.app']
-CSRF_TRUSTED_ORIGINS = [
-    "https://https://prepccjsom-production.up.railway.app",
-]
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
