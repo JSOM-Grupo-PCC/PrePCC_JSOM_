53152690e345c9cb7ff7236ae928e9bb958c2076
savioomio <saviopessoaafonso@gmail.com>
2024-01-01 05:13:22 -0300

    configurando ALLOWED_HOSTS

--- project/settings.py
+++ project/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 #(para o sistema em produção)
-ALLOWED_HOSTS = ['prepccjsom-production.up.railway.app']
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -136,3 +136,4 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+ALLOWED_HOSTS = ['prepccjsom-production.up.railway.app']
