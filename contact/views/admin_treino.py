ac699b696f9826be95c3953ea033d3c7b485922a
savioomio <saviopessoaafonso@gmail.com>
2024-01-15 13:36:52 -0300

    adicionando a lista de treinos dos alunos em seu perfil

--- contact/views/admin_treino.py
+++ contact/views/admin_treino.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render, get_object_or_404
-from contact.models import UserProfile
+from contact.models import UserProfile, Treino
 from django.utils import timezone
+
 
 @user_passes_test(lambda u: u.is_staff)
 def lista_alunos(request):
@@ -15,11 +16,11 @@ def lista_alunos(request):
 
     return render(request, 'contact/lista_alunos.html', context)
 
-# Adicione a nova view para o perfil do usuário
 @user_passes_test(lambda u: u.is_staff)
 def perfil_aluno(request, user_id):
     user_profile = get_object_or_404(UserProfile, user__id=user_id)
     site_title = f"Perfil do aluno {user_profile.user.username}"
+    categorias = Treino.CATEGORIA_CHOICES
 
     # Calcular idade a partir da data de nascimento
     if user_profile.data_nascimento:
@@ -28,10 +29,14 @@ def perfil_aluno(request, user_id):
     else:
         idade = None
 
+    treinos = Treino.objects.filter(owner=user_profile.user)
+
     context = {
         'user_profile': user_profile,
         'idade': idade,
         'site_title': site_title,
+        'treinos': treinos, 
+        'categorias': categorias,
     }
 
     return render(request, 'contact/perfil_aluno.html', context)
